8be94aef06dc3503efa824a34735c9d4b9aee663
Harshit-Dhatwalia-041023 <dhatwaliaharshit01@gmail.com>
2025-11-08 21:25:15 +0530

    Fix wildcard route for Express 5

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(methooverride("_method"));
 app.use(express.static(path.join(__dirname,"dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
